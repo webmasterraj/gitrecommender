ba67c2f14319086a7d23a83816c396e52ed5dffa
Niraj Sheth <nhsheth@gmail.com>
2016-02-28 19:22:09 -0800

    setting GIT_PER_PAGE from environment variable

--- git_details.py
+++ git_details.py
@@ -7,5 +7,6 @@ MAX_USERS = int(os.environ['GIT_MAX_USERS'])
 username = os.environ['GIT_USER']
 password = os.environ['GIT_PW']
 token = os.environ['GIT_TOKEN']
+PER_PAGE = os.environ['GIT_PER_PAGE'] # Setting per_page to max 100
 
-g = Github(token, per_page=100)
+g = Github(token, per_page=PER_PAGE)
