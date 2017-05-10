c6e1a4cf155f985dd1f3b709a7c5618942a1a236
Niraj Sheth <nhsheth@gmail.com>
2017-05-09 21:17:12 -0700

    Adapting models to Flask

--- models.py
+++ models.py
@@ -1,62 +1,83 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from settings import DATABASE
-
-Base = declarative_base()
-
 app = Flask(__name__)
-class Users(Base):
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRCK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column('user_id', Integer)
-    user_name = Column('user_name', String)
-    email = Column('email', String, nullable=True)
-    url = Column('url', String, nullable=True)
-    location = Column('location', String, nullable=True)
-    fullname = Column('fullname', String, nullable=True)
-    created_at = Column('created_at', DateTime, nullable=True)
-    random = Column('random', String, nullable=True)
-    num_stars = Column('num_stars', Integer, nullable=True)
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer())
+    user_name = db.Column(db.String())
+    email = db.Column(db.String(), nullable=True)
+    url = db.Column(db.String(), nullable=True)
+    location = db.Column(db.String(), nullable=True)
+    fullname = db.Column(db.String(), nullable=True)
+    created = db.Column(db.DateTime(), nullable=True)
+    num_stars = db.Column(db.Integer(), nullable=True)
+
+    def __init__(self, user_id, user_name, email=None, url=None, location=None, 
+            fullname=None, created=None, num_stars=None):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.email = email
+        self.location = location
+        self.fullname = fullname
+        self.created = created
+        self.num_stars = num_stars
 
 
-class Repos(Base):
+class Repo(db.Model):
     __tablename__ = "repos"
 
-    id = Column(Integer, primary_key=True)
-    repo_id = Column('repo_id', Integer)
-    repo_name = Column('repo_name', String)
-    owner_id = Column('owner_id', Integer)
-    owner_name = Column('owner_name', String)
-    name = Column('name', String, nullable=True)
-    num_forks = Column('num_forks', Integer, nullable=True)
-    num_stargazers = Column('num_stargazers', Integer, nullable=True)
-    topics = Column('topics', ARRAY(String), nullable=True)
+    id = db.Column(db.Integer(), primary_key=True)
+    repo_id = db.Column(db.Integer())
+    repo_name = db.Column(db.String())
+    owner_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    owner = db.relationship('User', backref='repos', lazy='dyanmic')
+    num_forks = db.Column(db.Integer(), nullable=True)
+    num_stargazers = db.Column(db.Integer(), nullable=True)
+    topics = db.Column(db.ARRAY(db.String()), nullable=True)
+
+    def __init__(self, repo_id, repo_name, owner_id, owner_name,
+            num_forks=None, num_stargazers=None, topics=None):
+        self.repo_id = repo_id
+        self.repo_name = repo_name
+        self.owner_id = owner_id
+        self.num_forks = num_forks
+        self.num_stargazers = num_stargazers
+        self.topics = topics
 
 
-class Stars(Base):
+class Star(db.Model):
     __tablename__ = "stars"
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column('user_id', Integer)
-    repo_id = Column('repo_id', Integer)
-    starred_at = Column('starred_at', DateTime)
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    repo_id = db.Column(db.Integer(), db.ForeignKey('repos.id'))
+    starred_at = db.Column(db.DateTime())
+
+    def __init__(self, user_id, repo_id, starred_at):
+        self.user_id = user_id
+        self.repo_id = repo_id
+        self.starred_at = starred_at
 
 
-class Clusters(Base):
+class Cluster(db.Model):
     __tablename__ = "clusters"
 
-    id = Column(Integer, primary_key=True)
-    cluster_id = Column('cluster_id', String)
-    seed = Column('seed', String)
-    repo_id = Column('repo_id', Integer, nullable=True)
-    created = Column('created', String, nullable=True)
-
-
-class ClusterMembership(Base):
-    __tablename__ = "user_cluster_rel"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column('user_id', Integer)
-    cluster_id = Column('cluster_id', String)
+    id = db.Column(db.Integer(), primary_key=True)
+    cluster_hash= db.Column(db.String())
+    seed = db.Column(db.String())
+    repo_id = db.Column(db.Integer(), db.ForeignKey('repo.id'))
+    created = db.Column(db.DateTime())
+    
+    def __init__(self, cluster_id, seed, repo_id, created):
+        self.cluster_id = cluster_id
+        self.seed = seed
+        self.repo_id = repo_id
+        self.created = created
